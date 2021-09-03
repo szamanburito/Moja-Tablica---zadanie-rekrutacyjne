b724dbe30a1bdd231057c71c0b629cd77c37e1ed
szamanburito <szamanburito@onet.pl>
2021-09-03 16:32:48 +0200

    Fixed counter of cards

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const AddBtn = document.querySelector(".Add--card--button");
 const main_container = document.getElementById("maincontainer");
 
-var card_number = 1;
-function counter() {
-  card_number++;
-}
+var card_number = 0;
 
 // Add new cards //
 AddBtn.addEventListener("click", AddNew);
-AddBtn.addEventListener("click", counter);
 
 function AddNew() {
+  card_number++;
+
   const Card = document.createElement("div");
   Card.classList.add("Card", "num--" + card_number);
 
@@ -33,6 +31,7 @@ function AddNew() {
     </div>`;
 
   main_container.appendChild(Card);
+  console.log(card_number);
 }
 
 
@@ -41,6 +40,7 @@ function AddNew() {
 function removeDiv(card_number) {
   const removeCard = document.querySelector(".num--" + card_number);
   removeCard.outerHTML = "";
+  console.log(card_number);
 }
 
 
